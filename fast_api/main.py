039fa25421dd535b84c11c8236a26bef03c5149e
David Alejandro Pineda Diaz <davidpineda1003@gmail.com>
2022-06-20 18:34:17 -0500

    request_response_body

---
+++ fast_api/main.py
@@ -0,0 +1,29 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastApi
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+@app.get('/')
+def home():
+    return {'hello': 'Wolrd'}
+
+# Request and Response body
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    return person
